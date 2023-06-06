5e7432539182f158e3a27ac4a621a195bceace76
Станислав Грибанов <youkillbird@gmail.com>
2023-06-06 16:11:10 +0500

    Add URL's validation

--- src/app.js
+++ src/app.js
@@ -1,27 +1,47 @@
 import './styles.scss';
 import '../index.html';
 
-import { object, string } from 'yup';
+import { string, setLocale } from 'yup';
+import view from './view.js';
 
-console.log('2');
+setLocale({
+  string: {
+    default: 'Ошибка данных, введите верный URL',
+    url: 'Это не URL',
+  },
+  mixed: {
+    notOneOf: 'Этот URL уже в списке фида. Введите другой адрес',
+  },
+});
 
 export default () => {
-    let userSchema = object({
-        url: string().url().nullable()
-    });
-    
-    const state = {};
-    
-    const form = document.querySelector('form');
-    form.addEventListener('submit', async (e) => {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const url = formData.get('url');
-        console.log(url);
-        const valid = await userSchema.validate({ url });
-        console.log(valid)
-    });
+  const form = document.querySelector('form');
+  const state = {
+    form: {
+      state: 'valid',
+      error: [],
+    },
+    feed: [],
+  };
 
-    console.log('2');
+  const watchedState = view(state);
 
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const userSchema = string().url().notOneOf(state.feed);
+
+    const formData = new FormData(e.target);
+    const url = formData.get('url');
+    try {
+      await userSchema.validate(url);
+      state.feed.push(url);
+      watchedState.form.error = '';
+      watchedState.form.state = 'valid';
+    } catch (error) {
+      watchedState.form.error = error.errors;
+      watchedState.form.state = 'invalid';
+      watchedState.form.state = '';
+      console.log(state);
+    }
+  });
 };
---
+++ src/view.js
@@ -0,0 +1,25 @@
+import onChange from 'on-change';
+
+export default (state) => {
+  const form = document.querySelector('form');
+  const input = document.querySelector('input');
+  const error = document.querySelector('#error');
+
+  const watchedState = onChange(state, (path, value) => {
+    if (path === 'form.state') {
+      console.log(state.feed);
+      if (value === 'invalid') {
+        error.textContent = state.form.error;
+        input.classList.add('is-invalid');
+        input.focus();
+      }
+      if (value === 'valid') {
+        error.textContent = state.form.error;
+        input.classList.remove('is-invalid');
+        form.reset();
+        input.focus();
+      }
+    }
+  });
+  return watchedState;
+};
