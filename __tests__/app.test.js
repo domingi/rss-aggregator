08ce11e2ff2ee3fd9f6a3d1105564ce655853268
Станислав Грибанов <youkillbird@gmail.com>
2023-10-05 16:33:00 +0500

    Fix linter comments

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,11 +9,8 @@ test.beforeEach(async ({ page }) => {
 
 test.afterEach(async ({ page }, testInfo) => {
   if (testInfo.status !== testInfo.expectedStatus) {
-    // Get a unique place for the screenshot.
     const screenshotPath = testInfo.outputPath('failure.png');
-    // Add it to the report.
     testInfo.attachments.push({ name: 'screenshot', path: screenshotPath, contentType: 'image/png' });
-    // Take the screenshot itself.
     await page.screenshot({ path: screenshotPath, timeout: 5000 });
   }
 });
@@ -23,8 +20,8 @@ test('has title', async ({ page }) => {
 });
 
 test('has main UI', async ({ page }) => {
-  await expect(page.getByText(/rss агрегатор/i)).toBeVisible();
-  await expect(page.getByText(/Начните читать RSS сегодня. Это легко, это красиво./i)).toBeVisible();
+  await expect(page.locator('text=/rss агрегатор/i')).toBeVisible();
+  await expect(page.locator('text=/Начните читать RSS сегодня. Это легко, это красиво./i')).toBeVisible();
   await expect(page.locator('button[type="submit"]')).toBeVisible();
 });
 
@@ -34,7 +31,7 @@ test('open feed', async ({ page }) => {
   await page.waitForTimeout(3000);
   await expect(page.locator('text=Фиды')).toBeVisible();
   await expect(page.locator('text=Посты')).toBeVisible();
-  await expect(page.getByText(/RSS успешно загружен/i)).toBeVisible();
+  await expect(page.locator('text=/RSS успешно загружен/i')).toBeVisible();
 });
 
 test('open modals', async ({ page }) => {
